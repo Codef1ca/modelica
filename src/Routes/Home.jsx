bf6e1551039d6d2187fedc8b945a2b79ce0f540f
sanmisanti <sanmisanti@gmail.com>
2023-05-23 16:52:13 -0300

    cambios en la pantalla al cambiar de ventana

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Navbar from '../Components/Navbar'
 import Hero from '../Components/Hero'
 import Benefits from "../Components/Beneficios/benefits";
@@ -6,6 +6,12 @@ import Lineas from "../Components/Lineas/lineas";
 import Testimonios from "../Components/Testimonios/testimonios";
 
 const Home = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+
   return (
     <div>
         <Navbar/>
